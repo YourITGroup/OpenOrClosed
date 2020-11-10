cafe520292a06656a380bfe900ab785b2e597bef
Robert Foster <robert@youritgroup.com.au>
2020-11-10 11:18:31 +1100

    updated sample project

--- OpenOrClosed/App_Plugins/OpenOrClosed/js/SpecialHours.controller.js
+++ OpenOrClosed/App_Plugins/OpenOrClosed/js/SpecialHours.controller.js
@@ -26,6 +26,13 @@
     $scope.model.config.time_24hr = Object.toBoolean($scope.model.config.time_24hr)
     $scope.model.config.showAppointmentOnly = Object.toBoolean($scope.model.config.showAppointmentOnly)
 
+    if ($scope.model.config.labelOpen.length === 0) {
+        $scope.model.config.labelOpen = 'Open'
+    }
+
+    if ($scope.model.config.labelClosed.length === 0) {
+        $scope.model.config.labelClosed = 'Closed'
+    }
     $scope.vm = {
         pickers: [],
         datePickerConfig: {
--- WebApplication/App_Plugins/OpenOrClosed/js/StandardHours.controller.js
+++ WebApplication/App_Plugins/OpenOrClosed/js/StandardHours.controller.js
@@ -8,6 +8,8 @@
         showBankHolidays: false,
         timeFormat: "HH:mm:ss",
         time_24hr: true,
+        labelOpen: 'Open',
+        labelClosed: 'Closed',
         icons: {
             time: "icon-time",
             up: "icon-chevron-up",
@@ -23,6 +25,13 @@
     $scope.model.config.showAppointmentOnly = Object.toBoolean($scope.model.config.showAppointmentOnly)
     $scope.model.config.showBankHolidays = Object.toBoolean($scope.model.config.showBankHolidays)
 
+    if ($scope.model.config.labelOpen.length === 0) {
+        $scope.model.config.labelOpen = 'Open'
+    }
+
+    if ($scope.model.config.labelClosed.length === 0) {
+        $scope.model.config.labelClosed = 'Closed'
+    }
     $scope.vm = {
         pickers: [],
         openTimePickerConfig: {
